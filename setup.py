329b7d8520e472406b17d82a62921591639e57c7
Joris Dobbelsteen <joris@familiedobbelsteen.nl>
2021-12-18 17:01:19 +0000

    distributed as version 0.1.0 to pypi

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     name='python-joris2k-ble',
     version='0.1.0',
     packages=['joris2k_ble'],
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=['bluepy>=1.0.5', 'click', 'construct', 'click-datetime'],
     description='Joris2k bluetooth low energy (BLE) devices library',
     author='Joris Dobbelsteen',
     author_email='joris@familiedobbelsteen.nl',
     maintainer='Joris Dobbelsteen',
     maintainer_email='joris@familiedobbelsteen.nl',
-    # url='https://github.com/rytilahti/python-eq3bt.git',
+    url='https://gitlab.com/jorisdobbelsteen/python-joris2k-ble/',
     license="MIT",
     entry_points={
         'console_scripts': [
